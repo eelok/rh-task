a07f8be00088daae73f5b8e6d60b7bfcb045814b
eelok <eelok@mail.ru>
2021-10-23 12:58:17 +0200

    add port

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ module.exports = {
         "password": process.env.PG_PASSWORD,
         "database": process.env.PG_DATABASE,
         "host": process.env.PG_HOST,
+        "port": process.env.PG_PORT,
         "logging": true,
         "dialect": "postgres"
     },
