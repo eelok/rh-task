4a30cca91e80f5998fb851502666cb75a5a99441
Maria <eelok@mail.ru>
2021-12-12 10:52:05 +0100

    wip

--- src/services/manufacturer.service.js
+++ src/services/manufacturer.service.js
@@ -4,4 +4,20 @@ const getAllManufacturers = () => Manufacturer.findAll();
 
 const getManufacturerById = (id) => Manufacturer.findByPk(id);
 
-module.exports = { getAllManufacturers, getManufacturerById };
+// const createManufacturer = async (manufacturer) => {
+//     const { name, location } = manufacturer;
+//     //TODO: Validation with Joi
+//     if (!name || !name.trim()) {
+//         throw new Error("manufacturer name is required");
+//     }
+//     if (await Manufacturer.findOne({ where: { name: name } })) {
+//         throw new Error(`Manufacturer with name ${name} already exists`);
+//     }
+//     return await Manufacturer.create(manufacturer);
+// };
+
+module.exports = {
+    getAllManufacturers,
+    getManufacturerById,
+    createManufacturer,
+};
